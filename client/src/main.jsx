ecc338e7584816d234a4b8bc9610538edbd95cd0
Arin Sharma <arinsharma.infinity@gmail.com>
2026-05-24 04:06:28 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-export const serverUrl = "https://optivest-backend.onrender.com";
+export const serverUrl = "https://optivest-ui.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
